40d61920748e9642446c655b8acf90df098ad4bf
Omer Mirza <omer.mirza.556@gmail.com>
2023-09-02 16:42:03 -0500

    Changed request message to environment variable

--- starter.py
+++ starter.py
@@ -1,10 +1,17 @@
+# Author: Omer Mirza
+# Date: 08/31/2023
+# Description: Simple Flask app that returns current time
+
 from flask import Flask, jsonify
 from time import time
+import os
 
-theMessage = "Automate all the things!" # Should I make this an env variable?
-
+# Passes MESSAGE environment variable to the request.
+# Allows for easier modification without App redeployment.
+theMessage = os.environ['MESSAGE']
 app = Flask(__name__)
 
+# Returns UNIX time when called by GET or POST request.
 @app.route('/', methods = ['GET', 'POST'])
 def returnTime():
     unixTime = int(time())
