54b4609066b7a8733cef2e6862cd1878be43d57f
Omer Mirza <omer.mirza.556@gmail.com>
2023-09-02 16:23:45 -0500

    API returns a message and timestamp

--- starter.py
+++ starter.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from time import time
+
+theMessage = "Automate all the things!" # Should I make this an env variable?
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World'
+@app.route('/', methods = ['GET', 'POST'])
+def returnTime():
+    unixTime = int(time())
+    return jsonify(
+        {
+            'message': theMessage,
+            'timestamp': unixTime
+        }
+    )
